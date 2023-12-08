5d75f4939a79ff0c3868ae273257350fb1029503
Kumar Aditya Raj <kumaradityaraj@Kumars-MacBook-Pro.local>
2023-12-08 19:39:04 +0530

    Corrected tailwind configuration

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import "./main.css";
 import { createPinia } from "pinia";
+import './assets/tailwind.css'
 
 const app = createApp(App);
 const pinia = createPinia();
