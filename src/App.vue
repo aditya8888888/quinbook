a03e013e6ffaccc265ec154ad34a38f25eafb2b6
Vishal Goyal <vishalgoyal@Vishals-MacBook-Pro.local>
2023-12-09 13:56:19 +0530

    Feed Component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
     <HeaderComponent></HeaderComponent>
+    <FeedComponent />
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 import HeaderComponent from './component/HeaderComponent.vue';
+import FeedComponent from "./component/FeedComponent.vue";
 export default defineComponent({
-  components: {HeaderComponent},
+  components: {HeaderComponent, FeedComponent},
   setup() { 
     return {
       
