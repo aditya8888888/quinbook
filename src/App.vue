6d0cd4991b0968bfaad3556b10fbdb0d090cfefc
Kumar Aditya Raj <kumaradityaraj@Kumars-MacBook-Pro.local>
2023-12-10 15:13:24 +0530

    Sign up Login Page

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
-  <NavBar />
-  <PostCard />
+  <!-- <NavBar />
+  <PostCard /> -->
+  <!-- <LoginPage /> -->
+  <SignUp />
 </template>
 
 <script>
-import NavBar from "./components/NavBar.vue";
-import PostCard from "./components/PostCard.vue";
+// import NavBar from "./components/NavBar.vue";
+// import PostCard from "./components/PostCard.vue";
+// import LoginPage from "./components/LoginPage.vue";
+import SignUp from "./components/SignUp.vue";
 export default {
   name: "App",
   components: {
-    NavBar,
-    PostCard,
+    // NavBar,
+    // PostCard,
+    // LoginPage,
+    SignUp,
   },
 };
 </script>
--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,14 +1,3 @@
-// import {
-//   Disclosure,
-//   DisclosureButton,
-//   DisclosurePanel,
-//   Menu,
-//   MenuButton,
-//   MenuItem,
-//   MenuItems,
-// } from "@headlessui/vue";
-// import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/vue/24/outline";
-
 export default {
   setup() {
     const navigation = [
